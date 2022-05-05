8b486bb0d90b1d2d61e59dc0f070c8419e4b3419
yakupkalayci <ykpklyc18@gmail.com>
2022-05-05 13:54:55 +0300

    design main page and add dark mode

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,30 +1,54 @@
-import {React, useState} from 'react'
-import AddContact from '../addContact/AddContact';
-import ContactList from '../contactList/ContactList';
+import { React, useState } from "react";
+import AddContact from "../addContact/AddContact";
+import ContactList from "../contactList/ContactList";
+import "./Home.css";
 
 function Home() {
-    const [windowStatus, setWindowStatus] = useState({addContact:false, showContacts:false});
+  const [windowStatus, setWindowStatus] = useState({
+    addContact: false,
+    showContacts: false,
+    showMessage : true
+  });
 
-    const handleClick = (e) => { 
-        setWindowStatus({addContact:false, showContacts:false});
-        setWindowStatus({
-            [e.target.name] : !windowStatus[e.target.name]
-        });
-    }
+  const [theme, setTheme] = useState("");
+
+  const handleClick = (e) => {
+    setWindowStatus({ addContact: false, showContacts: false });
+    setWindowStatus({
+      [e.target.name]: !windowStatus[e.target.name],
+    });
+  };
+
+  const changeTheme = () => {
+      setTheme(theme === "dark" ? "light" : "dark");
+  }
 
   return (
-    <div>
-        {
-        windowStatus.addContact ? <AddContact />
-        : 
-        windowStatus.showContacts ? <ContactList />
-        :
-        null
-        }
-        <button name='addContact' onClick={handleClick}>Add Contact</button>
-        <button name='showContacts' onClick={handleClick}>Show Contacts</button>
+    <div className={`Home ${theme}`}>
+        <div id="themeButton">
+            <button onClick={() => changeTheme()}>
+                {theme === "dark" ? "Light" : "Dark"}
+            </button>
+        </div>
+      <div id="displayPage">
+        {windowStatus.showMessage ? (
+            <h1>Welcome to the Contacts App!</h1>
+        ) : windowStatus.addContact ? (
+          <AddContact />
+        ) : windowStatus.showContacts ? (
+          <ContactList />
+        ) : null}
+      </div>
+      <div id="buttonGroups">
+        <button name="addContact" onClick={handleClick}>
+          Add Contact
+        </button>
+        <button name="showContacts" onClick={handleClick}>
+          Show Contacts
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
