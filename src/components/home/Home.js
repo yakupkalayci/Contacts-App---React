55496be56d254a7c4ca2ef23daf81f26992aeb89
yakupkalayci <ykpklyc18@gmail.com>
2022-05-05 13:06:26 +0300

    add ui logic

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,17 +3,26 @@ import AddContact from '../addContact/AddContact';
 import ContactList from '../contactList/ContactList';
 
 function Home() {
-    const [windowStatus, setWindowStatus] = useState({addContact:true, contacts:false});
+    const [windowStatus, setWindowStatus] = useState({addContact:false, showContacts:false});
+
+    const handleClick = (e) => { 
+        setWindowStatus({addContact:false, showContacts:false});
+        setWindowStatus({
+            [e.target.name] : !windowStatus[e.target.name]
+        });
+    }
 
   return (
     <div>
         {
         windowStatus.addContact ? <AddContact />
         : 
-        windowStatus.contacts ? <ContactList />
+        windowStatus.showContacts ? <ContactList />
         :
         null
         }
+        <button name='addContact' onClick={handleClick}>Add Contact</button>
+        <button name='showContacts' onClick={handleClick}>Show Contacts</button>
     </div>
   )
 }
